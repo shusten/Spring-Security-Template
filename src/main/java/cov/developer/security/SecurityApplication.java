1572e7c0f55f03f8bf3c83787c86be2c0fccb60a
Luis Villas-Boas <shusten@hotmail.com>
2024-09-09 04:53:39 -0300

    first commit

---
+++ src/main/java/cov/developer/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package cov.developer.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
