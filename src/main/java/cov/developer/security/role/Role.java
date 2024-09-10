3b7f417b86f746b6429fc3731b66abe6e711e5c3
Luis Villas-Boas <shusten@hotmail.com>
2024-09-09 23:56:53 -0300

    Add Role

---
+++ src/main/java/cov/developer/security/role/Role.java
@@ -0,0 +1,22 @@
+package cov.developer.security.role;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ROLE")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
