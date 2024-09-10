272ba113968c2feb73d3beb98e46203c3d0b7002
Luis Villas-Boas <shusten@hotmail.com>
2024-09-10 02:18:44 -0300

    :construction: Constructing User

--- src/main/java/cov/developer/security/user/UserController.java
+++ src/main/java/cov/developer/security/user/UserController.java
@@ -1,10 +1,20 @@
 package cov.developer.security.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService; //injeta a dependência do constructor UserService
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
 }
